c7aebf828a1de34a793b1549b5f854c422de3538
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-07-25 20:15:27 -0400

    final commit

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,8 +2,6 @@
 $(function() {
     $(".eat-burger").on("click", function(event) {
       var id = $(this).data("id");
-      
-  
       // Send the PUT request.
       $.ajax("/api/burger/" + id, {
         // type: "PUT"
@@ -12,7 +10,7 @@ $(function() {
       }).then(
         function() {
           console.log("updated burger #" + id);
-          // Reload the page to get the updated list, but first display a gif of devouring a buger
+          // Reload the page to get the updated list, but first display a quick gif of devouring a burger
           $(".gif-display").append("<img src='./assets/images/pulp_fiction_burger.gif'>")
           setTimeout(function() {
             location.reload();
