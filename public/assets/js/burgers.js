12ad2bbc9c8a1b523019aaeef4ec330016d3f499
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-07-25 20:05:19 -0400

    final commit

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,6 +2,7 @@
 $(function() {
     $(".eat-burger").on("click", function(event) {
       var id = $(this).data("id");
+      
   
       // Send the PUT request.
       $.ajax("/api/burger/" + id, {
@@ -11,12 +12,11 @@ $(function() {
       }).then(
         function() {
           console.log("updated burger #" + id);
-          // Reload the page to get the updated list
+          // Reload the page to get the updated list, but first display a gif of devouring a buger
           $(".gif-display").append("<img src='./assets/images/pulp_fiction_burger.gif'>")
           setTimeout(function() {
             location.reload();
-          }, 3000);
-          // location.reload();
+          }, 1360);
         }
       );
     });
@@ -24,23 +24,26 @@ $(function() {
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
-      var newBurger = {
+        var newBurger = {
         burger_name: $("#new-burger").val().trim(),
         devoured: false
       };
-  
-      // Send the POST request.
-      $.ajax("/api/burger", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+      if(newBurger.burger_name){
+
+        // Send the POST request.
+        $.ajax("/api/burger", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      }
+      else {
+        alert("Please enter the name of a burger.");
+      };
     });
   });
 
